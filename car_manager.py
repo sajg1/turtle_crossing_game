6e90863d6e87e2b884e046e17518b36095d9eebe
Steve Gossip <stephengossip@gmail.com>
2021-03-11 21:20:35 +0000

    worked on the car_manager class which generates a garage of cars that are released during the level_is_on loop. When the turtle reaches the end,  red_light() is called which hides the current cars and clears them from the garage list.

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,49 @@
+from turtle import Turtle
+import random
+import time
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_Y_COORD = [i for i in range(-220, 250, 20)]
+STARTING_X_COORD = [i for i in range(300, 3000, 10)]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+MOVE_INCREMENT = 2
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.garage = []
+        self.move_speed = STARTING_MOVE_DISTANCE
+
+    def generate_car(self):
+        new_car = Turtle()
+        new_car.shape('square')
+        new_car.penup()
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.color(random.choice(COLORS))
+        new_car.setheading(180)
+        new_car.speed(self.move_speed)
+        new_car.goto(random.choice(STARTING_X_COORD), random.choice(STARTING_Y_COORD))
+        self.garage.append(new_car)
+
+    def ready_cars(self):
+        self.generate_car()
+
+    def move_cars(self):
+        for car in self.garage:
+            car.forward(self.move_speed)
+
+    def green_light(self):
+        self.ready_cars()
+
+    def red_light(self):
+        for car in self.garage:
+            car.hideturtle()
+        self.garage = []
+
+    def car_accelerate(self):
+        self.move_speed += MOVE_INCREMENT
+
+
+
+
+
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ screen.tracer(0)
 
 scoreboard = Scoreboard()
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.move, 'Up')
@@ -18,10 +19,18 @@ screen.onkey(player.move, 'Up')
 
 game_is_on = True
 
+
 while game_is_on:
 
-    time.sleep(0.1)
-    screen.update()
-    if 270 < player.ycor() < 290:
-        scoreboard.levelup()
-        player.reset_position()
+    level_is_on = True
+    while level_is_on:
+        car_manager.green_light()
+        time.sleep(0.1)
+        screen.update()
+        car_manager.move_cars()
+        if 270 < player.ycor() < 300:
+            player.reset_position()
+            scoreboard.levelup()
+            car_manager.red_light()
+            car_manager.car_accelerate()
+            level_is_on = False
